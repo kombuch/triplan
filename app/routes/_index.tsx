1aba901dfb7a289657c1f0b338242b2eb7334e6c
Daniel Koch <dan@dkoch.dev>
2025-04-21 01:13:49 -0700

    update titles and desc and icon

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -4,8 +4,8 @@ import type { Route } from './+types/_index'
 // eslint-disable-next-line no-empty-pattern
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: 'New React Router App' },
-    { name: 'description', content: 'Welcome to React Router!' },
+    { title: 'Triplan' },
+    { name: 'description', content: 'Plan your next trip!' },
   ]
 }
 
